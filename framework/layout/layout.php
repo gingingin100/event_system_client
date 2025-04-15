88fc912ba54687de8544aa4ff518a07bdf18cef5
gingingin100 <43429647+gingingin100@users.noreply.github.com>
2025-04-15 19:35:05 -0400

    capitalisation

--- framework/layout/layout.php
+++ framework/layout/layout.php
@@ -4,6 +4,7 @@ namespace Framework\Layout;
 final class layout{
     public function render($view,$data =[]){
         extract($data);
+        
         require APP_DIR . '/views/partials/header.php';
         require APP_DIR . "/views/{$view}.php";
         require APP_DIR . '/views/partials/footer.php';
